06e47cffbc2485cec11d12c9ca2845e85b62a6d6
Erica-jihyeon <jihyeon20@gmail.com>
2022-02-13 12:10:15 -0600

    join room test complete-client

--- react_client/src/App.js
+++ react_client/src/App.js
@@ -1,13 +1,41 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import io from 'socket.io-client';
 import TextField from '@material-ui/core/TextField';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  const [state, setState] = useState({message:'', name:''});
-  const [chat,setChat] =useState([]);
+  const [state, setState] = useState({ message: '', name: '' });
+  const [chat, setChat] = useState([]);
   // const socketRef = useRef();
   const [socket, setSocket] = useState("");
+
+  const roomIdRef = useRef(null);
+
+  useEffect(() => {
+    const socket = io.connect('http://localhost:8080/matching');
+    setSocket(socket);
+
+    axios.get('http://localhost:8080/test')
+      .then((res) => {
+        // console.log(res.data.roomId);
+        roomIdRef.current = res.data.roomId;
+      })
+      .then(() => {
+        // console.log(roomIdRef.current);
+        socket.emit('joinRoom', { roomId: roomIdRef.current });
+        // socket.on("message", ({ name, message }) => {
+        //   setChat([...chat, { name, message }])
+        // });
+
+      })
+      .catch(err => {
+        console.log(err.message);
+      })
+
+    return () => socket.disconnect();
+
+  }, [])
 
   useEffect(()=>{
     // socketRef.current = io.connect('http://localhost:8080');
@@ -30,12 +58,10 @@ function App() {
     // socket.on('usercount', (data) => {
     //   console.log(data);
     // })
-
     const socket = io.connect('http://localhost:8080/matching');
     setSocket(socket);
 
-    socket.emit('joinRoom');
-    socket.on("message", ({ name, message }) => {
+    socket.on("message", ({ name, message, roomId }) => {
       setChat([ ...chat, { name, message } ])
     });
 
@@ -46,24 +72,24 @@ function App() {
 
   }, [chat])
 
-  
 
-  const onTextChange = e =>{
-    setState({...state,[e.target.name]: e.target.value})
+
+  const onTextChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value })
   }
 
-  const onMessageSubmit =(e)=>{
+  const onMessageSubmit = (e) => {
     e.preventDefault();
     //name = state.name, message = state.message
-    const {name, message} = state;
+    const { name, message } = state;
     // socketRef.current.emit('message',{name, message});
-    socket.emit('message',{name, message});
-    setState({message : '',name});
+    socket.emit('message', { name, message, roomId: roomIdRef.current });
+    setState({ message: '', name });
   }
 
 
-  const renderChat =()=>{
-    return chat.map(({name, message},index)=>(
+  const renderChat = () => {
+    return chat.map(({ name, message }, index) => (
       <div key={index}>
         <h3>{name}:<span>{message}</span></h3>
       </div>
@@ -75,20 +101,20 @@ function App() {
       <form onSubmit={onMessageSubmit}>
         <h1>Message</h1>
         <div className="name-field">
-          <TextField 
-          name ="name" 
-          onChange={e=> onTextChange(e)} 
-          value={state.name}
-          label="Name"/>
+          <TextField
+            name="name"
+            onChange={e => onTextChange(e)}
+            value={state.name}
+            label="Name" />
         </div>
         <div >
-          <TextField 
-          name ="message" 
-          onChange={e=> onTextChange(e)} 
-          value={state.message}
-          id="outlined-multiline-static"
-          variant="outlined"
-          label="Message"/>
+          <TextField
+            name="message"
+            onChange={e => onTextChange(e)}
+            value={state.message}
+            id="outlined-multiline-static"
+            variant="outlined"
+            label="Message" />
         </div>
         <button>Send Message</button>
       </form>
